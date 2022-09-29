90c03a63d831d49b72a476226b9e3eea1c5cebe8
Shalliinn <panchalshalin4300@gmail.com>
2022-09-29 17:22:27 +0530

    first

---
+++ dom_man/task3/1.js
@@ -0,0 +1,28 @@
+console.dir(document);
+console.log(document.title);
+console.log(document.URL);
+document.title= 123
+
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[10]);
+document.all[10].textContent='hello'
+console.log(document.forms);
+console.log(document.links);
+console.log(document.images);
+console.log(document.getElementById('header-title'));
+var headerTitle=document.getElementById('header-title');
+var header =document.getElementById('main-header')
+console.log(headerTitle);
+headerTitle.textContent='hello';
+headerTitle.innerText='goodbye';
+headerTitle.innerHTML='<h3>Hello</h3>';
+headerTitle.style.borderBottom= 'solid 3px #000'
+header.style.borderBottom='solid 3px #000';
+console.log(document.getElementsByClassName("title"));
+var head=document.getElementsByClassName("title");
+head[0].style.color='green'
+head[0].style.fontWeight='bold'
+
