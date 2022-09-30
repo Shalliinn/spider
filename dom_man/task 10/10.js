9aa2f15262d1d08ba240455a26358a8c9778a7d4
Shalliinn <panchalshalin4300@gmail.com>
2022-09-30 15:36:16 +0530

    new

---
+++ dom_man/task 10/10.js
@@ -0,0 +1,18 @@
+var namei=document.getElementById('name')
+var emaili=document.getElementById('email')
+localStorage.setItem('userdetailsname','name')
+localStorage.setItem('userdetailsemail','email')
+
+var myForm=document.querySelector('#my-form');
+myForm.addEventListener('submit',onsubmit);
+function onsubmit(e){
+    e.preventDefault();
+    if(namei.value==='' || emaili.value===''){
+        alert('please enter fields')
+    }
+    else{
+localStorage.setItem('userdetailsname',namei.value)
+localStorage.setItem('userdetailsemail',emaili.value)
+
+    }
+}
