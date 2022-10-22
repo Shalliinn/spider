bcf4eb1fe1ee29a02ab68c621dbfc4c04211b648
Shalliinn <panchalshalin4300@gmail.com>
2022-10-22 20:16:17 +0530

    TASK 9

---
+++ node-app/t-9/app2.js
@@ -0,0 +1,8 @@
+const httP=require('http');
+
+const routes=require('./routes')
+
+//const server=httP.createServer(routes);
+//const server=httP.createServer(routes.handler);
+
+server.listen(4000);
